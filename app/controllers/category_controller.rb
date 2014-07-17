c2a4f51cdb34a464304646b2d18ccd0ca309eb09
Ian Carnevale <ian.carnevale@gmail.com>
2014-07-17 15:12:44 -0400

    added bootstrap to bower dependencies

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,6 +1,6 @@
 class CategoryController < ApplicationController
   def index
   	@categories = Category.includes(:sub_category)
-  	render json: @categories, status: :ok
+  	render json: @categories.to_json(:include => :sub_category), status: :ok
   end
 end
