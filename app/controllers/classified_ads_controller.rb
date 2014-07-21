5b85d920041cea7a9556aef4b86a60e3a60172c6
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2014-07-21 17:49:00 -0400

    Added create action and added params

--- app/controllers/classified_ads_controller.rb
+++ app/controllers/classified_ads_controller.rb
@@ -3,4 +3,18 @@ class ClassifiedAdsController < ApplicationController
       @classified_ads = ClassifiedAd.all
       render json: @classified_ads, status: :ok
   end
+    
+  def new
+      @classified_ad = ClassifiedAd.new
+  end
+    
+  def create
+      @user = User.find(params[:user_id])
+      @classified_ad = @user.classified_ad.create(classified_ad_params)
+      redirect_to 'root'
+  end
+    
+  private
+    def classified_ad_params
+        params.require(:classified_ad).permit(:poster_name, :poster_email, :photo)
 end
