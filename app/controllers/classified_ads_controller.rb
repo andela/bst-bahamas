71b85a11a2c430a76abc6747674c7811004e19ad
Brice Nkengsa <ian.carnevale@gmail.com>
2014-07-23 12:18:34 -0400

    remove unused filter

--- app/controllers/classified_ads_controller.rb
+++ app/controllers/classified_ads_controller.rb
@@ -1,5 +1,4 @@
 class ClassifiedAdsController < ApplicationController
-  # skip_before_filter :verify_authenticity_token, :only => [:update]
   before_action :set_user, only: [:create, :update]
   before_action :set_classified_ad, only: [:update, :destroy]
 
